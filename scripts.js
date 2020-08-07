0a49e802e8f18e0e9cc6f66be475f083f1d81616
Paige <paigegryski@gmail.com>
2020-08-07 14:15:10 -0400

    Trivia Styling

---
+++ scripts.js
@@ -0,0 +1,419 @@
+//make it so answer only shows after 1 click//
+  function answerText() {
+    var text = document.getElementById("answer1");
+    if (text.style.display == "none"){
+      text.style.display = "block";
+    }
+    else {
+      text.style.display = "none";
+    }
+  }
+
+  document.getElementById("q1").addEventListener("click", function() {
+    answerText();
+    }, false);
+
+//////////
+
+function answerText2() {
+  var text = document.getElementById("answer2");
+  if (text.style.display == "none"){
+    text.style.display = "block";
+  }
+  else {
+    text.style.display = "none";
+  }
+}
+
+document.getElementById("q2").addEventListener("click", function() {
+  answerText2();
+  }, false);
+
+
+//////////
+function answerText3() {
+  var text = document.getElementById("answer3");
+  if (text.style.display == "none"){
+    text.style.display = "block";
+  }
+  else {
+    text.style.display = "none";
+  }
+}
+
+document.getElementById("q3").addEventListener("click", function() {
+  answerText3();
+  }, false);
+
+  //////////
+  function answerText4() {
+    var text = document.getElementById("answer4");
+    if (text.style.display == "none"){
+      text.style.display = "block";
+    }
+    else {
+      text.style.display = "none";
+    }
+  }
+
+  document.getElementById("q4").addEventListener("click", function() {
+    answerText4();
+    }, false);
+
+  //////////
+  function answerText5() {
+    var text = document.getElementById("answer5");
+    if (text.style.display == "none"){
+      text.style.display = "block";
+    }
+    else {
+        text.style.display = "none";
+    }
+  }
+
+  document.getElementById("q5").addEventListener("click", function() {
+    answerText5();
+    }, false);
+
+    //////////
+    function answerText6() {
+      var text = document.getElementById("answer6");
+      if (text.style.display == "none"){
+        text.style.display = "block";
+      }
+      else {
+        text.style.display = "none";
+      }
+    }
+
+    document.getElementById("q6").addEventListener("click", function() {
+      answerText6();
+      }, false);
+
+    //////////
+    function answerText7() {
+      var text = document.getElementById("answer7");
+      if (text.style.display == "none"){
+        text.style.display = "block";
+      }
+      else {
+          text.style.display = "none";
+      }
+    }
+    document.getElementById("q7").addEventListener("click", function() {
+      answerText7();
+      }, false);
+
+        function answerText8() {
+          var text = document.getElementById("answer8");
+          if (text.style.display == "none"){
+            text.style.display = "block";
+          }
+          else {
+            text.style.display = "none";
+          }
+        }
+
+        document.getElementById("q8").addEventListener("click", function() {
+          answerText8();
+          }, false);
+
+      //////////
+
+      function answerText9() {
+        var text = document.getElementById("answer9");
+        if (text.style.display == "none"){
+          text.style.display = "block";
+        }
+        else {
+          text.style.display = "none";
+        }
+      }
+
+      document.getElementById("q9").addEventListener("click", function() {
+        answerText9();
+        }, false);
+
+
+      //////////
+      function answerText3() {
+        var text = document.getElementById("answer3");
+        if (text.style.display == "none"){
+          text.style.display = "block";
+        }
+        else {
+          text.style.display = "none";
+        }
+      }
+
+      document.getElementById("q3").addEventListener("click", function() {
+        answerText3();
+        }, false);
+
+        //////////
+        function answerText4() {
+          var text = document.getElementById("answer4");
+          if (text.style.display == "none"){
+            text.style.display = "block";
+          }
+          else {
+            text.style.display = "none";
+          }
+        }
+
+        document.getElementById("q4").addEventListener("click", function() {
+          answerText4();
+          }, false);
+
+        //////////
+        function answerText5() {
+          var text = document.getElementById("answer5");
+          if (text.style.display == "none"){
+            text.style.display = "block";
+          }
+          else {
+              text.style.display = "none";
+          }
+        }
+
+        document.getElementById("q5").addEventListener("click", function() {
+          answerText5();
+          }, false);
+
+          //////////
+          function answerText6() {
+            var text = document.getElementById("answer6");
+            if (text.style.display == "none"){
+              text.style.display = "block";
+            }
+            else {
+              text.style.display = "none";
+            }
+          }
+
+          document.getElementById("q6").addEventListener("click", function() {
+            answerText6();
+            }, false);
+
+          //////////
+          function answerText7() {
+            var text = document.getElementById("answer7");
+            if (text.style.display == "none"){
+              text.style.display = "block";
+            }
+            else {
+                text.style.display = "none";
+            }
+          }
+          document.getElementById("q7").addEventListener("click", function() {
+            answerText7();
+            }, false);
+            //////////
+            function answerText8() {
+              var text = document.getElementById("answer8");
+              if (text.style.display == "none"){
+                text.style.display = "block";
+              }
+              else {
+                text.style.display = "none";
+              }
+            }
+
+            document.getElementById("q8").addEventListener("click", function() {
+              answerText8();
+              }, false);
+
+            //////////
+            function answerText9() {
+              var text = document.getElementById("answer9");
+              if (text.style.display == "none"){
+                text.style.display = "block";
+              }
+              else {
+                  text.style.display = "none";
+              }
+            }
+            document.getElementById("q9").addEventListener("click", function() {
+              answerText9();
+              }, false);
+              //////////
+
+              function answerText10() {
+                var text = document.getElementById("answer10");
+                if (text.style.display == "none"){
+                  text.style.display = "block";
+                }
+                else {
+                  text.style.display = "none";
+                }
+              }
+
+              document.getElementById("q10").addEventListener("click", function() {
+                answerText10();
+                }, false);
+
+
+              //////////
+              function answerText11() {
+                var text = document.getElementById("answer11");
+                if (text.style.display == "none"){
+                  text.style.display = "block";
+                }
+                else {
+                  text.style.display = "none";
+                }
+              }
+
+              document.getElementById("q11").addEventListener("click", function() {
+                answerText11();
+                }, false);
+
+//////////
+  function answerText12() {
+    var text = document.getElementById("answer12");
+    if (text.style.display == "none"){
+    text.style.display = "block";
+      }
+    else {
+    text.style.display = "none";
+    }
+    }
+
+  document.getElementById("q12").addEventListener("click", function() {
+  answerText12();
+  }, false);
+
+//////////
+  function answerText13() {
+    var text = document.getElementById("answer13");
+    if (text.style.display == "none"){
+    text.style.display = "block";
+    }
+    else {
+    text.style.display = "none";
+    }
+  }
+
+  document.getElementById("q13").addEventListener("click",function() {
+    answerText13();
+    }, false);
+
+//////////
+  function answerText14() {
+    var text = document.getElementById("answer14");
+    if (text.style.display == "none"){
+    text.style.display = "block";
+    }
+    else {
+    text.style.display = "none";
+  }
+  }
+
+document.getElementById("q14").addEventListener("click", function() {
+  answerText14();
+  }, false);
+
+  //////////
+    function answerText12() {
+      var text = document.getElementById("answer12");
+      if (text.style.display == "none"){
+      text.style.display = "block";
+        }
+      else {
+      text.style.display = "none";
+      }
+      }
+
+    document.getElementById("q12").addEventListener("click", function() {
+    answerText12();
+    }, false);
+
+  //////////
+    function answerText15() {
+      var text = document.getElementById("answer15");
+      if (text.style.display == "none"){
+      text.style.display = "block";
+      }
+      else {
+      text.style.display = "none";
+      }
+    }
+
+    document.getElementById("q15").addEventListener("click",function() {
+      answerText15();
+      }, false);
+
+  //////////
+    function answerText16() {
+      var text = document.getElementById("answer16");
+      if (text.style.display == "none"){
+      text.style.display = "block";
+      }
+      else {
+      text.style.display = "none";
+    }
+    }
+
+  document.getElementById("q16").addEventListener("click", function() {
+    answerText16();
+    }, false);
+
+    //////////
+      function answerText17() {
+        var text = document.getElementById("answer17");
+        if (text.style.display == "none"){
+        text.style.display = "block";
+          }
+        else {
+        text.style.display = "none";
+        }
+        }
+
+      document.getElementById("q17").addEventListener("click", function() {
+      answerText17();
+      }, false);
+
+    //////////
+      function answerText18() {
+        var text = document.getElementById("answer18");
+        if (text.style.display == "none"){
+        text.style.display = "block";
+        }
+        else {
+        text.style.display = "none";
+        }
+      }
+
+      document.getElementById("q18").addEventListener("click",function() {
+        answerText18();
+        }, false);
+
+    //////////
+      function answerText19() {
+        var text = document.getElementById("answer19");
+        if (text.style.display == "none"){
+        text.style.display = "block";
+        }
+        else {
+        text.style.display = "none";
+      }
+      }
+
+    document.getElementById("q19").addEventListener("click", function() {
+      answerText19();
+      }, false);
+
+      //////////
+        function answerText20() {
+          var text = document.getElementById("answer20");
+          if (text.style.display == "none"){
+          text.style.display = "block";
+          }
+          else {
+          text.style.display = "none";
+        }
+        }
+
+      document.getElementById("q20").addEventListener("click", function() {
+        answerText20();
+        }, false);
